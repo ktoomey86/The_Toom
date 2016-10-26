96a8e87d7d306cc0721fba60f2b1d33d225289cb
kyle_toomey <dsi.kyle.toomey@gmail.com>
2016-10-26 19:21:51 -0400

    uber_script

--- R_Scripts/sandbox.R
+++ R_Scripts/sandbox.R
@@ -11,3 +11,10 @@ abline(h=mean(sandbox))
 deviation<-sd(sandbox)
 abline(h=mean(sandbox)-deviation)
 abline(h=mean(sandbox)+deviation)
+
+data_file_park     <- data_file[data_file$InventoryTypeId != 1,]
+df2<- data.frame(neal= data_file_park$section)
+                 
+> df3<-grep("Blue", df2$neal, perl = TRUE, value = TRUE)
+> string<-paste0(df3, collapse = ",")
+> string
